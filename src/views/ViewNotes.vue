56cb88670bee444f40421691a62d2fdd70eaef6d
simone <simone.l.silva@parceiros.nos.pt>
2025-02-15 15:26:53 +0000

    chore: add card in view notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="notes">
-        <h1>Notes</h1>
+  <div class="notes">
+    <div v-for="i in 3" class="card">
+      <div class="card-content">
+        <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam vero, explicabo, consequuntur vel necessitatibus optio libero aspernatur iusto eius facere voluptates. Doloribus dicta quisquam consequatur, quam nemo minus id fugit.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
     </div>
+  </div>
 </template>
