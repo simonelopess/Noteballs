1f00b9f719587abbc7e122888f497321f850ab98
simone <simone.l.silva@parceiros.nos.pt>
2025-02-15 15:38:06 +0000

    chore: add a form in viewnotes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="notes">
+    <div class="card has-background-success p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"></textarea>
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link ">Add New Noe</button>
+        </div>
+      </div>
+    </div>
     <div v-for="i in 3" class="card">
       <div class="card-content">
         <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam vero, explicabo, consequuntur vel necessitatibus optio libero aspernatur iusto eius facere voluptates. Doloribus dicta quisquam consequatur, quam nemo minus id fugit.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+          vero, explicabo, consequuntur vel necessitatibus optio libero
+          aspernatur iusto eius facere voluptates. Doloribus dicta quisquam
+          consequatur, quam nemo minus id fugit.
         </div>
       </div>
       <footer class="card-footer">
