c5d566ad2d51ec66b4dd8b93ac22bc084e5ad61a
simone <simone.l.silva@parceiros.nos.pt>
2025-02-15 15:05:55 +0000

    create: navbar component using Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-  <RouterLink to="/stats">Stats</RouterLink>
+  <NavBar />
+  <!-- <RouterLink to="/">Notes</RouterLink>
+  <RouterLink to="/stats">Stats</RouterLink> -->
  <RouterView />
 </template>
+
+
+<script setup>
+  import NavBar from './components/Layout/NavBar.vue';
+</script>
+
+<style>
+  @import 'bulma/css/bulma.min.css';
+</style>
