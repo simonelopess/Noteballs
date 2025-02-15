3608fb797c1227e470b88a9839984c97e94cab84
simone <simone.l.silva@parceiros.nos.pt>
2025-02-15 15:13:47 +0000

    chore: add style in pages

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <NavBar />
   <!-- <RouterLink to="/">Notes</RouterLink>
   <RouterLink to="/stats">Stats</RouterLink> -->
- <RouterView />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
 
